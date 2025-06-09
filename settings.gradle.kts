9b1052ade5314a7b30655d73dbbebd5860078756
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-06-09 15:16:50 +0200

    Initial commit: add project skeleton, including build, license, and error-handling classes.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "alcosi-identity-server-communication"
+rootProject.name = "alcosi-identity-server-communication-error-parser"
 
